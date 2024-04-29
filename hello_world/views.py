30a5ffbd6bb40e9361203a69d7a788e31ac65d79
John Doherty <johndoherty1984@live.com>
2024-04-29 14:04:50 +0000

    Remove more commented code

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,12 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world!")
-
-# def index(request):
-#     if request.method == "POST":
-#         return HttpResponse("You must have POSTed something")
-#     else:
-#         return HttpResponse(request.method)
-    
+    return HttpResponse("Hello, world!")    
     
